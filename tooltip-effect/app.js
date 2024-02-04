61bdb3e969d0acaaad1f08453815dc8d79aa2c64
fl0ffy <bernat.johnathan@gmail.com>
2024-02-04 14:31:53 +0100

    populate app.js with initial content

---
+++ tooltip-effect/app.js
@@ -0,0 +1,17 @@
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    const helpTextList = [
+        {
+            id: home,
+            text: "You are already on home"
+        },
+        {
+            id: about,
+            text: "Get the info about us"
+        },
+        {
+            id: contact,
+            text: "Connect with us"
+        }
+    ]
+})
