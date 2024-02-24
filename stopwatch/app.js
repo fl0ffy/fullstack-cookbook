b2458e0cbcca78aa455d7cbc173bddb0336fe124
fl0ffy <bernat.johnathan@gmail.com>
2024-02-24 18:15:00 +0100

    add init function and create DOM element variables

---
+++ stopwatch/app.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", init);
+
+
+function init() {
+    var hours = document.getElementById('hours'),
+        minutes = document.getElementById('minutes'),
+        seconds = document.getElementById('seconds'),
+        milliseconds = document.getElementById('milliseconds'),
+
+        startButton = document.getElementById('startButton'),
+        pauseButton = document.getElementById('pauseButton'),
+        stopButton = document.getElementById('stopButton'),
+        resetButton = document.getElementById('resetButton');
+
+    
+
+}
